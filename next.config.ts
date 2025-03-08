f4d856d396e30268f59a698e2a774a55faafff55
Kesara Gamlath <66kesara99@gmail.com>
2025-03-08 22:02:42 +0530

    Next config ts file migrate

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/catch-the-stick-game',
+  assetPrefix: '/catch-the-stick-game/',
+  trailingSlash: true,
+  distDir: 'out',
 };
 
 export default nextConfig;
